96dc8709d3787c4bd9d92f6de812287a4d1db876
Michael L. Gantz <gantzm@gantzgulch.com>
2023-01-16 16:50:27 -0500

    Switched to using I2C for comms between left and right boards.

--- keyboards/handwired/gantzm/boomer/config.h
+++ keyboards/handwired/gantzm/boomer/config.h
@@ -5,16 +5,18 @@
 
 #define OLED_DISPLAY_128X64
 
+#define USE_I2C
 #define SPLIT_USB_DETECT 
-#define SOFT_SERIAL_PIN       D3
+
+//#define SOFT_SERIAL_PIN       D3
 
 // #define LED_CAPS_LOCK_PIN     D6
 
 #define MATRIX_COLS           10
 #define MATRIX_ROWS           12
 
-#define MATRIX_COL_PINS       { B5, F0, B6, B2, B3, B1, F7, F6, F5, F4} 
-#define MATRIX_ROW_PINS       { D2, D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS       { B4, B5, B6, B2, B3, B1, F7, F6, F5, F4} 
+#define MATRIX_ROW_PINS       { D3, D2, D4, C6, D7, E6 }
 
-#define MATRIX_COL_PINS_RIGHT { B5, F0, B6, B2, B3, B1, F7, F6, F5, F4} 
-#define MATRIX_ROW_PINS_RIGHT { D2, D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS_RIGHT { B4, B5, B6, B2, B3, B1, F7, F6, F5, F4} 
+#define MATRIX_ROW_PINS_RIGHT { D3, D2, D4, C6, D7, E6 }
